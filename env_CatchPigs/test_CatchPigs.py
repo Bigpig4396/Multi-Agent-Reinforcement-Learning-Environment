af2376bd11690e57ba47ad22a14ba8e3bc6a96ac
Shuo Jiang <jiangshuo_sd@126.com>
2018-10-17 20:04:16 -0400

    update

--- env_CatchPigs/test_CatchPigs.py
+++ env_CatchPigs/test_CatchPigs.py
@@ -3,8 +3,25 @@ import random
 import numpy as np
 
 env = EnvCatchPigs()
+'''
 max_iter = 100
 action1_list = np.random.randint(0, 4, size=[1, max_iter])
 action2_list = np.random.randint(0, 4, size=[1, max_iter])
 action_pig_list = np.random.randint(0, 3, size=[1, max_iter])
 env.test(action1_list, action2_list, action_pig_list, max_iter, 1000, True)
+'''
+
+
+max_iter = 100
+for i in range(max_iter):
+    print("iter= ", i)
+    env.step(random.randint(0, 4), random.randint(0, 4), random.randint(0, 3))
+    print("agent 1 is at ", env.agt1_pos)
+    print("agent 1 is looking at ", env.agt1_ori)
+    print("agent 1 observation= ", env.get_agt1_obs())
+    print("agent 2 is at ", env.agt2_pos)
+    print("agent 2 is looking at ", env.agt2_ori)
+    print("agent 2 observation= ", env.get_agt2_obs())
+    print("pig is at ", env.pig_pos)
+    print("pig is looking at ", env.pig_ori)
+    print("pig observation= ", env.get_pig_obs())
--- env_FindGoals/test_FindGoals.py
+++ env_FindGoals/test_FindGoals.py
@@ -3,7 +3,17 @@ import random
 import numpy as np
 
 env = EnvFindGoals()
-max_iter = 1000
+'''max_iter = 1000
 action1_list = np.random.randint(0, 4, size=[1, max_iter])
 action2_list = np.random.randint(0, 4, size=[1, max_iter])
-env.test(action1_list, action2_list, max_iter, 200, True)
+env.test(action1_list, action2_list, max_iter, 100, True)
+'''
+
+max_iter = 100
+for i in range(max_iter):
+    print("iter= ", i)
+    env.step(random.randint(0, 4), random.randint(0, 4))
+    print("agent 1 is at ", env.agent1_pos)
+    print("agent 1 observation= ", env.get_agt1_obs())
+    print("agent 2 is at ", env.agent2_pos)
+    print("agent 2 observation= ", env.get_agt2_obs())
